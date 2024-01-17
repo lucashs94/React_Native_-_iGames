25bcb6349ae9f1e1360281c722f99c5c9d1b584e
lucashs94 <h7.lucas@gmail.com>
2024-01-17 17:36:13 -0300

    adding custom splash screen and its controll

--- App.tsx
+++ App.tsx
@@ -1,18 +1,41 @@
 import { StatusBar } from 'react-native'
 import { ThemeProvider } from 'styled-components/native'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet'
 import { useFonts, Poppins_400Regular, Poppins_700Bold } from '@expo-google-fonts/poppins'
+import * as SplashScreen from 'expo-splash-screen'
 
 import theme from './src/styles/theme'
-import { Routes } from './src/routes';
-import { Loading } from './src/components/Loading';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 
+import { Routes } from './src/routes'
+import { Loading } from './src/components/Loading'
+import { useEffect } from 'react'
 
+SplashScreen.preventAutoHideAsync()
 
 export default function App() {
 
   const [fontsLoaded] = useFonts({ Poppins_400Regular, Poppins_700Bold })
+
+
+  useEffect(() => {
+
+    async function prepare(){
+      try {
+        await new Promise(resolve => setTimeout(resolve, 4000))
+        console.log('passou aqui')
+        
+      } catch (error) { 
+        console.log(error)
+
+      }finally{
+        await SplashScreen.hideAsync()
+      }
+    }
+
+    prepare()
+  }, [])
+
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
